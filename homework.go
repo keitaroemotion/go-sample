2d969e6544a26d0f5b16ac4d03ecd9b254f4ff4d
Kei Sugano <kei.sugano@h-t.co.jp>
2020-06-21 16:34:09 +0900

    sum of squares successful

--- homework.go
+++ homework.go
@@ -6,25 +6,25 @@ const SECRET_FILE = "/usr/etc/hmt/credentials"
 const SECRET_KEY  = "/usr/etc/hmt/.secret"
 
 func main() {
-    var x = filterNegativeElements([]int{3, -1, 1, 14}, []int{}, 0)
-    fmt.Println(x)
+    var positiveNumbers = getPositiveNumbers([]int{3, -1, 1, 14}, []int{}, 0)
+    var sumOfSquares    = getSumOfSquares(positiveNumbers, 0)
+    fmt.Println(sumOfSquares)
 }
 
-func filterNegativeElements(numbers [] int, positiveNumbers []int, cursor int) []int {
+func getPositiveNumbers(numbers [] int, positiveNumbers []int, cursor int) []int {
     if(cursor == len(numbers)) { return positiveNumbers }
     var number = numbers[cursor]
     if(number > 0) {
         positiveNumbers = append(positiveNumbers, number)
     }
-    return filterNegativeElements(numbers, positiveNumbers, cursor + 1)
+    return getPositiveNumbers(numbers, positiveNumbers, cursor + 1)
 }
 
 func getSumOfSquares(numbers []int, sum int) int {
-    var head = numbers[0]
-    sum      = sum + head * head
-    if(len(numbers) == 1) {
+    if(len(numbers) == 0) {
         return sum
     }
+    var head = numbers[0]
     var tail = numbers[1:]
-    return head + getSumOfSquares(tail, sum)
+    return getSumOfSquares(tail, sum + (head * head))
 }
